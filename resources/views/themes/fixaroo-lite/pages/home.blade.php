d5d2d0386f8c7feb3b6788ca3d7904c1daab3425
sivuyile.shasha <sivuyile.shasha@vml.com>
2025-03-05 19:41:23 +0200

    Add more templates

---
+++ resources/views/themes/fixaroo-lite/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Welcome to the Home Page</h1>
+    <p>This is a clean light-themed template for Laravel 8.</p>
+@endsection
---
+++ resources/views/themes/fixaroo-lite/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <p>&copy; {{ date('Y') }} All rights reserved.</p>
+</footer>
