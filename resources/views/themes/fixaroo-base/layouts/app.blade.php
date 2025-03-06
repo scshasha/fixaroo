28f58b7a33ab4ce5836fcffd6a3c23a7fcefc88c
sivuyile.shasha <sivuyile.shasha@vml.com>
2025-03-06 02:26:36 +0200

    Implement base theme

--- resources/views/themes/fixaroo-base/layouts/app.blade.php
+++ resources/views/themes/fixaroo-base/layouts/app.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <title>{{ config('app.name') }} | @yield('title', 'Welcome')</title>
+
+    <!-- Load CSS -->
+    <link rel="stylesheet" href="{{ mix('themes/fixaroo-base/css/app.css') }}">
 </head>
 <body>
+<!-- Include Navbar -->
 @include('themes::fixaroo-base.components.navbar')
 
+<!-- Main Content -->
 <div class="container">
     @yield('content')
 </div>
 
+<!-- Include Footer -->
 @include('themes::fixaroo-base.partials.footer')
+
+<!-- Load Combined JS -->
+<script src="{{ mix('themes/fixaroo-base/js/all.js') }}"></script>
 </body>
 </html>
